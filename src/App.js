bf37a2f6e9578dc35c0aa057246c0196bfd7d8ea
unknown <constancekwameabbey@gmail.com>
2023-04-22 13:06:27 +0100

    Add Note component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Note from "./components/Note";
 
 function App() {
   return (
     <div>
       <Header />
+      <Note />
       <Footer />
     </div>
   );
---
+++ src/components/Note.js
@@ -0,0 +1,9 @@
+export default function Note() {
+
+    return (
+        <div className="note">
+            <h1>This is the title</h1>
+            <p>This is the content</p>
+        </div>
+    )
+}
